3afe5b3755bfd94c45b616b3b66c8b5a1fe5042a
Rajesh000730 <rajeshrachit09@gmail.com>
2024-01-01 22:31:42 +0530

    lexer test

--- go/lexer/lexer_test.go
+++ go/lexer/lexer_test.go
@@ -6,5 +6,28 @@ import(
 )
 
 func TestNextToken(t *testing.T){
-  input
+  input := `=+(){},;`
+
+  tests := []struct{
+    expectedType token.TokenType
+    expectedLiteral string
+  }{
+    {token.ASSIGN, "="},
+    {token.PLUS, "+"},
+    {token.LPARAN, "("},
+    {token.RPAREN, ")"},
+    {token.LBRACE, "{"},
+    {token.RBRACE, "}"},
+    {token.COMMA, ","},
+    {token.EOF, ""},
+    {token.SEMICOLON, ";"},
+  }
+
+  l := New(input)
+  for i, tt := range tests{
+    tok := l.NextToken()
+    if tok.type != tt.expectedType{
+      t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%d", i, tt.expectedType, tok.type)
+    }
+  }
 }
